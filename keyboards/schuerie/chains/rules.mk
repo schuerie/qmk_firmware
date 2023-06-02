88f8b0b0832da2edd1c5aa91e30e8eede4bff51c
Stefan Baumgartner <schuerie@protonmail.com>
2023-06-02 21:04:16 +0200

    testing build

--- keyboards/schuerie/chains/rules.mk
+++ keyboards/schuerie/chains/rules.mk
@@ -10,4 +10,4 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = no
